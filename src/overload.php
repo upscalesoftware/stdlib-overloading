198fff4d57cdd7e7199a5d7d0b2c4aeb96012466
Sergii Shymko <sergey@shymko.net>
2020-02-12 17:49:18 -0800

    Introduce overloading function

---
+++ src/overload.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace Upscale\Stdlib\Overloading;
+
+/**
+ * Return closure delegating calls to the first compatible implementation
+ *
+ * @param callable[] $implementations
+ * @return callable
+ * @throws \LogicException|\Error
+ */
+function overload(callable ...$implementations): callable
+{
+    return function (...$args) use ($implementations) {
+        $error = new \LogicException('Invalid overloaded implementations');
+        foreach ($implementations as $candidate) {
+            try {
+                return $candidate(...$args);
+            } catch (\TypeError $e) {
+                $error = $e;
+            }
+        }
+        throw $error;
+    };
+}
